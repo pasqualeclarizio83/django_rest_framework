30a956c89f04e495fe4cdaa6b8ced5a31b7707e3
pasqualeclarizio83 <pascalclarizio@gmail.com>
2024-01-05 10:26:58 +0100

    API RestFul and API View

--- api_rest_base/myapi/first_app/api/views.py
+++ api_rest_base/myapi/first_app/api/views.py
@@ -2,6 +2,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 
 from rest_framework.permissions import AllowAny
+from collections.abc import MutableMapping
 
 @api_view()
 @permission_classes([AllowAny])
